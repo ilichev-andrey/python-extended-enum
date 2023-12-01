3c0e8c70415f881948192811e004ba8c8f35643a
andreyilichev <ilichev.andrey.y@gmail.com>
2023-12-02 00:44:28 +0300

    Add comparison test

---
+++ tests/test_comparison.py
@@ -0,0 +1,82 @@
+from uuid import UUID
+
+from extended_enum import ExtendedEnum, BaseExtendedEnumValue, ValueWithDescription
+
+
+class MixedEnum1(ExtendedEnum):
+    """A combined enumeration in which member values are of different types."""
+
+    CONST1 = 'const1'
+    CONST2 = 1
+    CONST3 = UUID('79ff3431-3e98-4bec-9a4c-63ede2580f83')
+    NOT_DUPLICATE_CONST3 = '79ff3431-3e98-4bec-9a4c-63ede2580f83'
+    CONST4 = BaseExtendedEnumValue(value='const4')
+    CONST5 = BaseExtendedEnumValue(value=2)
+    CONST6 = BaseExtendedEnumValue(value=UUID('e7b4b8ae-2224-47ec-afce-40aeb10b85e2'))
+    CONST7 = ValueWithDescription(value='const7')
+    CONST8 = ValueWithDescription(value=3, description='some const8 description')
+
+
+class MixedEnum2(ExtendedEnum):
+    """A combined enumeration in which member values are of different types."""
+
+    CONST1 = 'const1'
+    CONST2 = 1
+    CONST3 = UUID('79ff3431-3e98-4bec-9a4c-63ede2580f83')
+    NOT_DUPLICATE_CONST3 = '79ff3431-3e98-4bec-9a4c-63ede2580f83'
+    CONST4 = BaseExtendedEnumValue(value='const4')
+    CONST5 = BaseExtendedEnumValue(value=2)
+    CONST6 = BaseExtendedEnumValue(value=UUID('e7b4b8ae-2224-47ec-afce-40aeb10b85e2'))
+    CONST7 = ValueWithDescription(value='const7')
+    CONST8 = ValueWithDescription(value=3, description='some const8 description')
+
+
+def test_different_classes():
+    """
+    Comparison of identical values from different classes is checked.
+    Expected:
+        - Identical values located in different classes are NOT equal
+    """
+
+    for item1 in MixedEnum1:
+        item2 = getattr(MixedEnum2, item1.name)
+        assert item1 == item1
+        assert item1.value == item2.value
+        assert item1 != item2
+
+
+def test_different_origins():
+    """
+    Comparison of identical values of different origins is checked.
+    Expected:
+        - Values that do not belong to the same class are NOT equal.
+    """
+
+    origins = {
+        'CONST1': 'const1',
+        'CONST2': 1,
+        'CONST3': UUID('79ff3431-3e98-4bec-9a4c-63ede2580f83'),
+        'NOT_DUPLICATE_CONST3': '79ff3431-3e98-4bec-9a4c-63ede2580f83',
+        'CONST4': 'const4',
+        'CONST5': 2,
+        'CONST6': UUID('e7b4b8ae-2224-47ec-afce-40aeb10b85e2'),
+        'CONST7': 'const7',
+        'CONST8': 3,
+    }
+    for item in MixedEnum1:
+        assert item != item.value
+        origin = origins[item.name]
+        assert item.value == origin
+        assert item != origin
+
+    origins = {
+        'CONST4': BaseExtendedEnumValue(value='const4'),
+        'CONST5': BaseExtendedEnumValue(value=2),
+        'CONST6': BaseExtendedEnumValue(value=UUID('e7b4b8ae-2224-47ec-afce-40aeb10b85e2')),
+        'CONST7': ValueWithDescription(value='const7'),
+        'CONST8': ValueWithDescription(value=3, description='some const8 description'),
+    }
+    for name, origin in origins.items():
+        item = getattr(MixedEnum1, name)
+        assert item.value == origin.value
+        assert item != origin
